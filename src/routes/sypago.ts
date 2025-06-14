97596180d9258822ee27360548593a2565b3a2e7
Eng. Demin Santana <deminsantana@gmail.com>
2025-06-13 20:26:20 -0400

    feat: remove OTP endpoint from sypago routes

--- src/routes/sypago.ts
+++ src/routes/sypago.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { tasaBank, bankOptions, sypagoAuth, sypagoOtpRequest } from '../controllers/sypago.controller';
+import { tasaBank, bankOptions, sypagoAuth } from '../controllers/sypago.controller';
 
 const router = Router();
 
 router.post('/auth', sypagoAuth);
 router.get('/bankOptions', bankOptions);
 router.get('/tasa', tasaBank);
-router.post('/otp/sypago', sypagoOtpRequest);
 
 export default router;
