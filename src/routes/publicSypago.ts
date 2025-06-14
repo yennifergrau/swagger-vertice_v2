e10276edd08302f930432c918e613f5afa2f00e9
Eng. Demin Santana <deminsantana@gmail.com>
2025-06-13 20:25:47 -0400

    feat: add OTP endpoint to publicSypago routes and remove from sypago routes

--- src/routes/publicSypago.ts
+++ src/routes/publicSypago.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { bankOptions, tasaBank } from '../controllers/sypago.controller';
+import { bankOptions, tasaBank, sypagoOtpRequest } from '../controllers/sypago.controller';
 
 const router = Router();
 
 router.get('/bankOptions', bankOptions);
 router.get('/tasa', tasaBank);
+router.post('/otp/sypago', sypagoOtpRequest); // Agregar el endpoint OTP en la raíz
 
 export default router;
