7b35cd9701d12b71167aad4d7fe847f8dd13f59b
Eng. Demin Santana <deminsantana@gmail.com>
2025-06-17 12:19:47 -0400

    update: 2 /verify (PLACA)

--- src/controllers/plate.controller.ts
+++ src/controllers/plate.controller.ts
@@ -1,38 +1,41 @@
-
 import { Request, Response } from 'express';
 import pool from '../config/db';
 
 export const verifyPlateCtrl = async (req: Request, res: Response) => {
   try {
     const {
-      policy_holder_type_document,
-      policy_holder_document_number,
+      // policy_holder_type_document,
+      // policy_holder_document_number,
       plate,
     } = req.body;
 
     if (
-      !policy_holder_type_document  ||
-      !policy_holder_document_number ||
-      !plate 
+      // !policy_holder_type_document ||
+      // !policy_holder_document_number ||
+      !plate
     ) {
       return res.status(400).json({ message: 'Faltan datos requeridos' });
     }
 
-    const [cars]: any = await pool.query('SELECT car_id FROM cars WHERE plate = ?', [plate]);
-    if (!Array.isArray(cars) || cars.length === 0) {
-      return res.json({ message: 'La placa no existe' });
-    }
-    const carId = cars[0].car_id;
-
-    
-    const [orders]: any = await pool.query(
-      'SELECT car_id FROM orders WHERE car_id = ?',
-      [carId]
+    const [policies]: any = await pool.query(
+      `
+      SELECT
+        p.policy_id
+      FROM
+        policies p
+      JOIN
+        cars c ON p.car_id = c.car_id
+      WHERE
+        c.plate = ?
+      `,
+      [plate]
     );
-    if (Array.isArray(orders) && orders.length > 0) {
-      return res.json({ message: 'Ya existe un registro con esta placa' });
+
+    if (Array.isArray(policies) && policies.length > 0) {
+      return res.json({ message: 'El Vehículo tiene póliza' });
+    } else {
+      return res.json({ message: 'El Vehículo no tiene póliza' });
     }
-    return res.json({ message: 'La placa no existe' });
   } catch (error) {
     return res.status(500).json({
       message: 'Error interno del servidor al verificar placa',
@@ -40,4 +43,3 @@ export const verifyPlateCtrl = async (req: Request, res: Response) => {
     });
   }
 };
-
